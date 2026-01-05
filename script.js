d3a6820a2b5efdc10de97f0a76e226a7be2f3d4a
fiedorbartlomiej5-hue <fiedorbartlomiej5@gmail.com>
2026-01-05 11:47:07 +0100

    Dodano stronę wizytówkę

---
+++ script.js
@@ -0,0 +1,19 @@
+function powitanie() {
+    alert("Witaj na mojej stronie wizytówkowej!");
+}
+
+function pokazWyksztalcenie() {
+    alert("Wykształcenie: wyższe");
+}
+
+function pokazDoswiadczenie() {
+    alert("Doświadczenie: podstawowe");
+}
+
+function pokazZainteresowania() {
+    alert("Zainteresowania: tenis ziemny, piłka nożna");
+}
+
+function pokazKontakt() {
+    alert("Email: bartlomiej.fiedor@example.com\nTelefon: 600 123 456");
+}
